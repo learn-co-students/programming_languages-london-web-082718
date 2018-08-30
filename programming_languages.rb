f2e3b51b57fcd2bf028efbd8ae6fb95bb6f386ac
braj011 <bhagyaraj01@gmail.com>
2018-08-30 16:06:00 +0100

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,16 @@
+require 'pry'
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |style, value|
+    value.each do |language|
+      # value.each creates several arrays from our original hash. Our hash does not have arrays so this
+      # was unexpected.
+      if new_hash[language[0]]
+        new_hash[language[0]][:style] << style
+      else
+      new_hash[language[0]] = {language[1], :style => [style]}
+      end
+    end
+  end
+  p new_hash
 end
